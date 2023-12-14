c635dd964f40db548a124cf5a02051d22f8384d2
Archit Singh <contact.architsingh@gmail.com>
2023-12-14 06:03:34 +0530

    add agent which uses tools for different functionalities

---
+++ oai_app/agent_with_tools.py
@@ -0,0 +1,186 @@
+import autogen
+
+config_list_gpt = autogen.config_list_from_json("OAI_CONFIG_LIST")
+
+llm_config = {
+    "cache_seed": 42,  # change the cache_seed for different trials
+    "temperature": 0,
+    "config_list": config_list_gpt,
+    "timeout": 120,
+}
+
+# Import things that are needed generically
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.tools import BaseTool
+from typing import Type
+import math
+import os
+
+
+class CircumferenceToolInput(BaseModel):
+    radius: float = Field()
+
+
+class CircumferenceTool(BaseTool):
+    name = "circumference_calculator"
+    description = "Use this tool when you need to calculate a circumference using the radius of a circle"
+    args_schema: Type[BaseModel] = CircumferenceToolInput
+
+    def _run(self, radius: float):
+        return float(radius) * 2.0 * math.pi
+
+
+def get_file_path_of_example():
+    # Get the current working directory
+    current_dir = os.getcwd()
+
+    # Go one directory up
+    parent_dir = os.path.dirname(current_dir)
+
+    # Move to the target directory
+    target_folder = os.path.join(parent_dir, "test")
+
+    # Construct the path to your target file
+    file_path = os.path.join(target_folder, "test_files/radius.txt")
+
+    return file_path
+
+
+from langchain.tools.file_management.read import ReadFileTool
+
+
+# Define a function to generate llm_config from a LangChain tool
+def generate_llm_config(tool):
+    # Define the function schema based on the tool's args_schema
+    function_schema = {
+        "name": tool.name.lower().replace(" ", "_"),
+        "description": tool.description,
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": [],
+        },
+    }
+
+    if tool.args is not None:
+        function_schema["parameters"]["properties"] = tool.args
+
+    return function_schema
+
+
+# Instantiate the ReadFileTool
+read_file_tool = ReadFileTool()
+custom_tool = CircumferenceTool()
+
+# Construct the llm_config
+llm_config = {
+    # Generate functions config for the Tool
+    "functions": [
+        generate_llm_config(custom_tool),
+        generate_llm_config(read_file_tool),
+    ],
+    "config_list": config_list_gpt,  # Assuming you have this defined elsewhere
+    "timeout": 120,
+}
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    is_termination_msg=lambda x: x.get("content", "")
+    and x.get("content", "").rstrip().endswith("TERMINATE"),
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    code_execution_config={"work_dir": "coding"},
+)
+
+# Register the tool and start the conversation
+user_proxy.register_function(
+    function_map={
+        custom_tool.name: custom_tool._run,
+        read_file_tool.name: read_file_tool._run,
+    }
+)
+
+chatbot = autogen.AssistantAgent(
+    name="chatbot",
+    system_message="For coding tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.",
+    llm_config=llm_config,
+)
+
+user_proxy.initiate_chat(
+    chatbot,
+    message=f"Read the file with the path {get_file_path_of_example()}, then calculate the circumference of a circle that has a radius of that files contents.",  # 7.81mm in the file
+    llm_config=llm_config,
+)
+
+# Starndard Langchain example
+from langchain.agents import create_spark_sql_agent
+from langchain.agents.agent_toolkits import SparkSQLToolkit
+from langchain.chat_models import ChatOpenAI
+from langchain.utilities.spark_sql import SparkSQL
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+schema = "langchain_example"
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {schema}")
+spark.sql(f"USE {schema}")
+csv_file_path = "./sample_data/california_housing_train.csv"
+table = "california_housing_train"
+spark.read.csv(csv_file_path, header=True, inferSchema=True).write.option(
+    "path",
+    "file:/content/spark-warehouse/langchain_example.db/california_housing_train",
+).mode("overwrite").saveAsTable(table)
+spark.table(table).show()
+
+# Note, you can also connect to Spark via Spark connect. For example:
+# db = SparkSQL.from_uri("sc://localhost:15002", schema=schema)
+spark_sql = SparkSQL(schema=schema)
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+toolkit = SparkSQLToolkit(db=spark_sql, llm=llm)
+agent_executor = create_spark_sql_agent(llm=llm, toolkit=toolkit, verbose=True)
+
+# Starndard Langchain example
+agent_executor.run("Describe the california_housing_train table")
+
+
+# Now use AutoGen with Langchain Tool Bridgre
+tools = []
+function_map = {}
+
+for tool in toolkit.get_tools():  # debug_toolkit if you want to use tools directly
+    tool_schema = generate_llm_config(tool)
+    print(tool_schema)
+    tools.append(tool_schema)
+    function_map[tool.name] = tool._run
+
+# Construct the llm_config
+llm_config = {
+    "functions": tools,
+    "config_list": config_list,  # Assuming you have this defined elsewhere
+    "timeout": 120,
+}
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    is_termination_msg=lambda x: x.get("content", "")
+    and x.get("content", "").rstrip().endswith("TERMINATE"),
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    code_execution_config={"work_dir": "coding"},
+)
+
+print(function_map)
+
+# Register the tool and start the conversation
+user_proxy.register_function(function_map=function_map)
+
+chatbot = autogen.AssistantAgent(
+    name="chatbot",
+    system_message="For coding tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.",
+    llm_config=llm_config,
+)
+
+user_proxy.initiate_chat(
+    chatbot,
+    message="Describe the table names california_housing_train",
+    llm_config=llm_config,
+)
